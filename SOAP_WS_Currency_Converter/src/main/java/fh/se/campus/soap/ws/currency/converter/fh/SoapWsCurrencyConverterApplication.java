59a85a75b6e12717728aab9f9515779f1e6bfb9f
cajlakem <39443209+cajlakem@users.noreply.github.com>
2021-02-20 03:26:56 +0100

    Initial Angular

---
+++ SOAP_WS_Currency_Converter/src/main/java/fh/se/campus/soap/ws/currency/converter/fh/SoapWsCurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package fh.se.campus.soap.ws.currency.converter.fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWsCurrencyConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapWsCurrencyConverterApplication.class, args);
+    }
+
+}
---
+++ SOAP_WS_Currency_Converter/src/test/java/fh/se/campus/soap/ws/currency/converter/fh/SoapWsCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.se.campus.soap.ws.currency.converter.fh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsCurrencyConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
