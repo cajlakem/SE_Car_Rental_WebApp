ab15aa99546e439c311cb72905892e871a08839d
cajlakem <39443209+cajlakem@users.noreply.github.com>
2021-03-26 00:30:08 +0100

    Swagger Doc https://localhost:8443/swagger/swagger-ui/index.html?configUrl=/swagger/api-docs/swagger-config

--- Car_Rental_WS/src/main/java/fh/se/car/rental/fh/CarRentalRESTWebService.java
+++ Car_Rental_WS/src/main/java/fh/se/car/rental/fh/CarRentalRESTWebService.java
@@ -7,8 +7,13 @@ import fh.se.car.rental.fh.model.enums.CurrencyCode;
 import fh.se.car.rental.fh.repository.CarRepository;
 import fh.se.car.rental.fh.repository.UserRepository;
 import fh.se.car.rental.fh.security.JWTAuthorizationFilter;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -42,8 +47,21 @@ public class CarRentalRESTWebService {
                     .authorizeRequests()
                     .antMatchers(HttpMethod.POST, "/api/v1/users/login").permitAll()
                     .antMatchers(HttpMethod.POST, "/api/v1/users/register").permitAll()
+                    .antMatchers(HttpMethod.GET, "/swagger/**").permitAll()
                     .anyRequest().authenticated();
         }
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                                .title("FH Campus Rental Car Web Service API")
+                                .version("1.0")
+                                .description("SE Engineering Project rules!!")
+                                .contact(new Contact().name("Emir Cajlakovic").email("emir.cajlakovic@nirvana.com"))
+                                .termsOfService("http://swagger.io/terms/")
+                                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 
     @Bean
