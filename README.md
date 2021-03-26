97378e514282bb99beb4067c351ce7f1bac1b402
cajlakem <39443209+cajlakem@users.noreply.github.com>
2021-03-26 23:48:15 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # SE Car Rental WebApp
 
-- Web Service 1: Car Rental 
+- Web Service 1: Car Rental (REST Java Spring Boot)
   - REST-Style
   - User registration, list cars, book & return cars
   - Support different currencies 
   - Store prices in USD 
   - Authentication
-- Web Service 2: Currency Converter 
+- Web Service 2: Currency Converter (C# SOAP)
   - SOAP/WSDL-Style
   - Data retrieval: http://www.ecb.europa.eu/stats/eurofxref/eurofxrefdaily.xml
   - Cross-Rates calculation required
   - Authentication
 
-- Web Service 3 
+- Web Service 3 (Google Service REST)
   - Use one of the Google maps web services 
     - https://developers.google.com/maps/documentation/webservices/
--- create.sql
+++
@@ -1,9 +0,0 @@
-create table hibernate_sequence (next_val bigint) engine=InnoDB
-insert into hibernate_sequence values ( 1 )
-create table users (id bigint not null, first_name varchar(255), last_name varchar(255), user_name varchar(255), primary key (id)) engine=InnoDB
-create table hibernate_sequence (next_val bigint) engine=InnoDB
-insert into hibernate_sequence values ( 1 )
-create table users (id bigint not null, first_name varchar(255), last_name varchar(255), user_name varchar(255), primary key (id)) engine=InnoDB
-create table hibernate_sequence (next_val bigint) engine=InnoDB
-insert into hibernate_sequence values ( 1 )
-create table users (id bigint not null, first_name varchar(255) not null, last_name varchar(255) not null, user_name varchar(255) not null, primary key (id)) engine=InnoDB
