c8dd8bc8f293524fc95dcedb4c93edd724f717db
Verena <matzenberger.verena@gmail.com>
2021-03-30 17:26:18 +0200

    add car list and styling

--- CarRentalAngularApp/src/app/home/car-list/car-list.component.ts
+++ CarRentalAngularApp/src/app/home/car-list/car-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CarService} from "../../_services/car-service";
+import {Car} from "../../_models/Car";
+import { faCheckDouble, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-car-list',
@@ -7,13 +10,21 @@ import {CarService} from "../../_services/car-service";
   styleUrls: ['./car-list.component.css']
 })
 export class CarListComponent implements OnInit {
+  cars:Car[] = [];
+  iconDoubleCheck = faCheckDouble;
+  iconCircleX = faTimesCircle;
 
   constructor(private carService: CarService) { }
 
   ngOnInit(): void {
     this.carService.retrieveCars().subscribe(cars => {
+      this.cars = cars;
       console.log(cars);
+      console.log(this.cars);
     });
+  }
 
+  isCarFree(status: string): boolean {
+    return status === 'FREE';
   }
 }
