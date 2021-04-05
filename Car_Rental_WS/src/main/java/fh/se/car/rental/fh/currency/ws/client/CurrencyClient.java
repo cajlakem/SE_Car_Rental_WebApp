1bc6a5cee2224f60caa7c7e41423b4e99f2cbf03
cajlakem <39443209+cajlakem@users.noreply.github.com>
2021-04-05 04:24:46 +0200

    SOAP Client Integration

--- Car_Rental_WS/src/main/java/fh/se/car/rental/fh/currency/ws/client/CurrencyClient.java
+++ Car_Rental_WS/src/main/java/fh/se/car/rental/fh/currency/ws/client/CurrencyClient.java
@@ -2,14 +2,21 @@ package fh.se.car.rental.fh.currency.ws.client;
 
 import fh.se.car.rental.fh.currency.ws.client.helper.Convert;
 import fh.se.car.rental.fh.currency.ws.client.helper.ConvertResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class CurrencyClient extends WebServiceGatewaySupport {
+
+    @Autowired
+    private Environment env;
+
 
     public ConvertResponse convertCurrency (String toCurrency, Double amount){
         Convert request = new Convert();
         request.setToCurrency(toCurrency);
         request.setAmount(amount);
+        request.setApiToken(env.getProperty("carrental.apiToken"));
         ConvertResponse response = (ConvertResponse) getWebServiceTemplate().marshalSendAndReceive(request);
         return response;
     }
