bde3ff6a0c9d7dee5cfae51093b1a61bd84aa3d8
cajlakem <39443209+cajlakem@users.noreply.github.com>
2021-04-05 04:57:51 +0200

    Cleanup

--- SOAP_WS_Currency_Converter/src/main/java/fh/se/campus/soap/ws/currency/converter/fh/helper/ExchangeRateParser.java
+++
@@ -1,9 +0,0 @@
-package fh.se.campus.soap.ws.currency.converter.fh.helper;
-
-/**
- * @author Andrej Kuročenko <kurochenko@mail.muni.cz>
- */
-public interface ExchangeRateParser {
-    public ExchangeRateDTO parseAll();
-    public ExchangeRateDTO parseActual();
-}
